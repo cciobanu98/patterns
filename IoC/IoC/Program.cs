6100ed4dfa13521a196e9353f16a7f3294688815
cciobanu98 <cristian.ciobanu1998@gmail.com>
2019-03-05 12:49:06 +0200

    dsa

---
+++ IoC/IoC/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+namespace IoC
+{
+     interface ILogin
+     {
+          void Login(User u);
+     }
+     class User
+     {
+          public string Name { get; set; }
+          public int Id { get; set; }
+          public ILogin Login { get; set; }
+          public User(string name, int id, ILogin login)
+          {
+               Name = name;
+               Id = id;
+               Login = login;
+          }
+          public void LoginUserInApp()
+          {
+               Login.Login(this);
+          }
+     }
+     class LoginUser : ILogin
+     {
+          public void Login(User u)
+          {
+               Console.WriteLine($"Login {u.Name} from DB");
+          }
+     }
+     class LoginFacebook : ILogin
+     {
+          public void Login(User u)
+          {
+               Console.WriteLine($"Login {u.Name} from Facebook");
+          }
+     }
+     class LoginGoogle : ILogin
+     {
+          public void Login(User u)
+          {
+               Console.WriteLine($"Login {u.Name} from Google");
+          }
+     }
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               List<User> db = new List<User>();
+               db.Add(new User("Cristian", 1, new LoginGoogle()));
+               db.Add(new User("Angela", 2, new LoginFacebook()));
+               db.Add(new User("Oleg", 3, new LoginUser()));
+               foreach (var u in db)
+                    u.LoginUserInApp();
+               Console.ReadKey();
+          }
+     }
+}
